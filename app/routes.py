5393d631f5a2263a83cf082b867fb9fcad406a71
lucascr91 <lucas.ecomg@gmail.com>
2021-12-25 19:43:27 -0300

    add code to retrieve wallet address at server side

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,15 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, url_for, request
+from web3 import Web3
+
+
+bsc = "https://bsc-dataseed.binance.org/"
+web3 = Web3(Web3.HTTPProvider(bsc))
+
+# print(web3.isConnected())
+# #read the block number
+# print(web3.eth.blockNumber)
+
 
 @app.route('/')
 @app.route('/index')
@@ -8,4 +18,19 @@ def index():
 
 @app.route('/checkWallet', methods=['GET', 'POST'])
 def checkWallet():
-    return render_template('checkWallet.html')
+    if request.method=="POST":
+        address = request.form.get("address")
+        kwargs = {
+            'address': address
+        }
+        result = bnb_balance(address)
+        print(result)
+    else:
+        kwargs={}
+
+    return render_template('checkWallet.html', **kwargs)
+
+def bnb_balance(address:str):
+    balance = web3.eth.getBalance(address.strip())
+    return web3.fromWei(balance, 'ether')
+
--- sm.py
+++ sm.py
@@ -1,1 +1,2 @@
 from app import app
+# from web3 import Web3
